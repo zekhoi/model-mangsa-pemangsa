2326bc2ed940fbf053cffa80a456ea0160c341ed
Khoironi Kurnia Syah <55708473+zekhoi@users.noreply.github.com>
2021-04-05 20:33:33 +0800

    1st commit

---
+++ potretfase.m
@@ -0,0 +1,32 @@
+% Potret Fase Model Mangsa Pemangsa
+
+clear all; close all; clc
+[x1, x2] = meshgrid(-0.2:0.2:3, -0.2:.2:3);
+
+% Plot isoclines dan lintasan bidang fase tertutup untuk memodelkan sistem persamaan Mangsa-Pemangsa Lotka-Volterra:
+x1dot = x1 - x2 .*x1;
+x2dot = x1 .* x2 - x2;
+
+% Membuat plot vektor dengan quiver
+figure (1);
+quiver(x1,x2,x1dot, x2dot, 'r')
+
+
+% Mendefinisikan model Mangsa-Pemangsa Lotka-Volterra System
+f = @(t,y) [y(1) - y(1)*y(2); y(1)*y(2) - y(2)];
+
+hold on
+
+% Menghitung fase potrait untuk kondisi awal yang berbeda dan plot lintasan fase dari loop tertutup
+for y0=0:.7:2.8
+	[ts, ys] = ode45(f,[0, 8], [y0/2, y0]);
+
+	plot(ys(:,1), ys(:,2),'k','Linewidth',2.0)
+	plot(ys(1,1),ys(1,2),'go','Linewidth',2.0) % starting point
+	plot(ys(end,1),ys(end,2),'rs','Linewidth',2.0) % ending point
+end
+
+hold off
+
+xlabel('x')
+ylabel('y')
---
+++ solusi.m
@@ -0,0 +1,32 @@
+ % Solusi Model Mangsa Pemangsa
+
+% Membersihkan window
+clear all; close all; clc
+
+% Mengimpor file / module bawaan 'lotka' dari Matlab
+type lotka
+
+% Menggunakan ode45/ode23 untuk menyelesaikan persamaan diferensial yang ditentukan dalam 'lotka'
+t0 = 0;
+tfinal = 15;
+y0 = [20; 20];
+[t,y] = ode45(@lotka,[t0 tfinal],y0);
+
+
+% Membuat plot populasi terhadap waktu
+figure (1);
+plot(t,y)
+title('Populasi Mangsa-Pemangsa Terhadap Waktu')
+xlabel('Waktu')
+ylabel('Populasi')
+legend('Mangsa','Pemangsa','Location','North')
+
+
+% Membuat plot populasi satu sama lain yang membuat hubungan siklik antar populasi menjadi jelas
+figure (2);
+plot(y(:,1),y(:,2))
+title('Phase Plane Plot')
+xlabel('Populasi Mangsa')
+ylabel('Populasi Pemangsa')
+
+
